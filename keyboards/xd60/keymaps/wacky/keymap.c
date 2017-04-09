ebde1ad3a99896eebe98b78e46b30a1500160b28
wacky6.AriesMBP <416707889@qq.com>
2017-04-09 17:55:47 +0800

    fix Fn0 -> Up/Down -> PgUp/PgDn, add ctrl-enter

--- keyboards/xd60/keymaps/wacky/keymap.c
+++ keyboards/xd60/keymaps/wacky/keymap.c
@@ -10,7 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,    KC_LBRC,  KC_RBRC,           KC_BSLS,   \
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT,  KC_NO,             KC_ENT,    \
       KC_LSFT, KC_NO,   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, KC_UP,    KC_DEL,    \
-      KC_NO,   KC_LGUI, KC_LALT,                             KC_SPC,                       KC_RALT, F(0),     KC_LEFT, KC_DOWN,  KC_RIGHT),
+      KC_NO,   KC_LGUI, KC_LALT,                             KC_SPC,                       KC_RCTL, F(0),     KC_LEFT, KC_DOWN,  KC_RIGHT),
 
   //
   // TODO: Windows ALT Layer to emulate Mac OS X Cmd+? keys
@@ -31,9 +31,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KEYMAP(
       KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_F13,   KC_DEL,    \
       F(16),   KC_WH_U, KC_UP,   KC_WH_D, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_MPRV,  KC_MNXT,           KC_TRNS,   \
-      KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT,KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,           KC_TRNS,   \
-      KC_LSFT, KC_NO,   KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLD, KC_VOLU, KC_MUTE,  KC_TRNS, KC_PGDN,  KC_TRNS,   \
-      KC_TRNS, KC_TRNS, KC_TRNS,                          F(31),                                KC_TRNS, F(0),     KC_HOME, KC_PGUP,  KC_END),
+      KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT,KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,           LCTL(KC_ENTER),   \
+      KC_LSFT, KC_NO,   KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLD, KC_VOLU, KC_MUTE,  KC_TRNS, KC_PGUP,  KC_TRNS,   \
+      KC_TRNS, KC_TRNS, KC_TRNS,                          F(31),                                KC_TRNS, F(0),     KC_HOME, KC_PGDN,  KC_END),
 
 };
 
