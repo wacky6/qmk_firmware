c296db11c286fd34c58fe9c4ceac9bb0ce1371ba
Jiewei Qian <qjw@wacky.one>
2018-05-28 22:15:53 +1000

    xd60-wacky: add x3 super repair toggle

--- keyboards/xd60/keymaps/wacky/keymap.c
+++ keyboards/xd60/keymaps/wacky/keymap.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //
   // TODO: Windows ALT Layer to emulate Mac OS X Cmd+? keys
   //       May consume lots of FN keys ??
-  // 
+  //
 
   // 1: Base Layer, Mac OS
   #define MAC_LAYER (1)
@@ -32,13 +32,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_F13,   KC_DEL,    \
       F(16),   KC_WH_U, KC_UP,   KC_WH_D, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_MPRV,  KC_MNXT,           KC_TRNS,   \
       KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT,KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,           LCTL(KC_ENTER),   \
-      KC_LSFT, KC_NO,   KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLD, KC_VOLU, KC_MUTE,  KC_TRNS, KC_PGUP,  KC_TRNS,   \
+      KC_LSFT, KC_NO,   KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLD, KC_VOLU, KC_MUTE,  KC_TRNS, KC_PGUP,  F(8),   \
       KC_TRNS, KC_TRNS, KC_TRNS,                          F(31),                                KC_TRNS, F(0),     KC_HOME, KC_PGDN,  KC_END),
 
 };
 
 enum function_id {
     ESC_TILD,
+    TOGGLE_X3_REPAIR,
     BASE_LAYER_MAC,
     INTELI_LEFT,
     INTELI_RIGHT,
@@ -48,6 +49,7 @@ enum function_id {
 // Custom Actions
 const uint16_t PROGMEM fn_actions[] = {
     [0] = ACTION_LAYER_MOMENTARY(FN_LAYER),    // Toggle Fn overlay
+    [8] = ACTION_FUNCTION(TOGGLE_X3_REPAIR),   // Toggle Ctrl / Ctrl-J Press (Play X3 Happily!)
     [16] = ACTION_FUNCTION(BASE_LAYER_MAC),    // Set base layer to mac
     [24] = ACTION_FUNCTION(ESC_TILD),
     [31] = ACTION_FUNCTION(TOGGLE_BACKLIGHT)
@@ -81,7 +83,8 @@ uint8_t determine_backlit_base(void) {
 void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
     static uint8_t registered_esc_tild;
-    switch (id) { 
+    static uint8_t x3_super_repair = 0;
+    switch (id) {
         case ESC_TILD:
             if (record->event.pressed) {
                 registered_esc_tild = IS_SHIFT ? KC_GRV : KC_ESC;
@@ -100,6 +103,26 @@ void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
         case TOGGLE_BACKLIGHT:
             if (record->event.pressed) {
                 rgblight_toggle();
+            }
+        break;
+        case TOGGLE_X3_REPAIR:
+            // Ctrl is alway pressed (Fire repair laser)
+            // If Alt is pressed when toggling on, also hold J (SETA Acceleration)
+            if (record->event.pressed) {
+                if (!x3_super_repair) {
+                    register_code(KC_LCTL);
+                    x3_super_repair = 1;
+                    if (get_mods() & MODS_ALT_MASK) {
+                        register_code(KC_J);
+                        x3_super_repair = 2;
+                    }
+                } else {
+                    unregister_code(KC_LCTL);
+                    if (x3_super_repair & 2) {
+                        unregister_code(KC_J);
+                    }
+                    x3_super_repair = 0;
+                }
             }
         break;
     }
